c264179f150033aa87b94eb60f27d71993f74fcd
jwh1308 <67500007+jwh1308@users.noreply.github.com>
2024-07-10 09:00:30 +0900

    [feature] page 25

--- prime_factor.py
+++ prime_factor.py
@@ -1,3 +1,5 @@
 class PrimeFactor:
     def of(self, param):
+        if param == 2:
+            return [2]
         return []
--- test_prime_factor.py
+++ test_prime_factor.py
@@ -7,3 +7,7 @@ class TestPrimeFactor(TestCase):
     def test_prime_factor(self):
         prime_factor = PrimeFactor()
         self.assertEqual(prime_factor.of(1), [])
+
+    def test_prime_factor_2(self):
+        prime_factor = PrimeFactor()
+        self.assertEqual(prime_factor.of(2), [2])
